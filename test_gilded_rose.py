295c167f23ae4d9edca57fce835075c961b4c512
csanann <csanann@gmail.com>
2023-06-21 18:59:27 +0100

    2nd test passing

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -11,3 +11,9 @@ def test_update_quality():
   gilded_rose.update_quality()
   
   assert items[0].quality == 6, "Quality of 'Elixir of the Mongoose' should decrease by 1"
+
+def test_aged_brie_increase_in_quality():
+  items = [Item("Aged Brie", sell_in = 2, quality = 0)]
+  gilded_rose = GildedRose(items)
+  gilded_rose.update_quality()
+  assert items[0].quality == 1, "Quality of 'Aged Brie' should increase by 1"
