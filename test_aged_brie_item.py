61d3eb84d74c8635a7d55a5376b84913df0d2353
csanann <csanann@gmail.com>
2023-06-23 12:49:16 +0100

    Adds 1st test, pytest passing

---
+++ test_aged_brie_item.py
@@ -0,0 +1,12 @@
+#file: test_aged_brie_item.py
+
+from aged_brie_item import AgedBrieItem
+from gilded_rose import GildedRose
+
+def test_aged_brie_item():
+  items = [AgedBrieItem("Aged Brie", sell_in = 2, quality = 0)]
+  gilded_rose = GildedRose(items)
+  gilded_rose.update_quality()
+  assert items[0].quality == 1, "Quality of 'Aged Brie' should increase by 1"
+  assert items[0].sell_in == 1, "Sell in days should decrease by 1"
+
