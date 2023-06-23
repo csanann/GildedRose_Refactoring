9787d783cae9580010e5c05fe23b2fb881abf611
csanann <csanann@gmail.com>
2023-06-23 12:52:48 +0100

    Adds 2nd test, pytest passing

--- test_aged_brie_item.py
+++ test_aged_brie_item.py
@@ -10,3 +10,8 @@ def test_aged_brie_item():
   assert items[0].quality == 1, "Quality of 'Aged Brie' should increase by 1"
   assert items[0].sell_in == 1, "Sell in days should decrease by 1"
 
+def test_aged_brie_item_quality_max():
+  items = [AgedBrieItem("Aged Brie", sell_in = 2, quality = 50)]
+  gilded_rose = GildedRose(items)
+  gilded_rose.update_quality()
+  assert items[0].quality == 50, "Quality of 'Aged Brie' should not exceed 50"
