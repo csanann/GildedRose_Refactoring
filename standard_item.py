892e01e98a628a4eb117e4b02fcfd9ac8e13d6b9
csanann <csanann@gmail.com>
2023-06-23 12:38:59 +0100

    Adds new class with 1st implmentation,pytest passing

---
+++ standard_item.py
@@ -0,0 +1,13 @@
+#file: standard_item.py
+
+from gilded_rose import Item
+
+class StandardItem(Item):
+  def update_quality(self):
+    if self.sell_in > 0:
+      self.quality -= 1
+    else:
+      self.quality -= 2
+    self.sell_in -= 1
+    if self.quality < 0:
+      self.quality = 0
