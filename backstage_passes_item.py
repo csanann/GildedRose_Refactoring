b9990c471bd43b9f9f6e7d67b832c01f73023fb9
csanann <csanann@gmail.com>
2023-06-23 13:07:28 +0100

    Adds new class, pytest passing

---
+++ backstage_passes_item.py
@@ -0,0 +1,23 @@
+#file: backstage_passes_item.py
+
+from standard_item import StandardItem
+
+# file: backstage_passes_item.py
+
+class BackstagePassesItem:
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def update_quality(self):
+        self.sell_in -= 1
+        if self.sell_in > 10:
+            self.quality += 1
+        elif self.sell_in <= 10 > 5:
+            self.quality += 2
+        elif self.sell_in <= 5:
+            self.quality += 3
+
+        self.quality = min(self.quality, 50)
+        self.quality = max(self.quality, 0)
