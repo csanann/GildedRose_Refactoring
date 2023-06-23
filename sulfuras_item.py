2e062b719e1f500210bea81fbc9ca8b99165b13a
csanann <csanann@gmail.com>
2023-06-23 13:32:35 +0100

    Adds new class, pytest passing

---
+++ sulfuras_item.py
@@ -0,0 +1,10 @@
+#file: sulfuras_item.py
+
+class SulfurasItem:
+  def __init__(self, name, sell_in, quality):
+    self.name = name
+    self.sell_in = sell_in
+    self.quality = quality
+
+  def update_quality(self):
+    pass
