691fe58ff4fabfd2fed65d2ff1e261f79dd65c8b
csanann <csanann@gmail.com>
2023-06-28 21:09:58 +0100

    Adds more test to improve coverage- same % result, test passing

--- tests/test_standard_item.py
+++ tests/test_standard_item.py
@@ -10,3 +10,10 @@ def test_standard_item():
   gilded_rose.update_quality()
   assert items[0].quality == 19, "Quality should decrease by 1"
   assert items[0].sell_in == 14, " Sell in days should decrease by 1"
+  
+def test_standard_item_quality_decreases_twice_as_fast_past_sell_date():
+  items = [StandardItem("foo", sell_in = 0, quality = 20)]
+  gilded_rose = GildedRose(items)
+  gilded_rose.update_quality()
+  assert items[0].quality == 18, "Quality should decrease by 2 when sell_in is less than 0"
+  assert items[0].sell_in == -1, "Sell in days should decrease by 1"
