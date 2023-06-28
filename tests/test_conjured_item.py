4429fa0df09dac1ee8b72dc0dceb87d6210f7fd0
csanann <csanann@gmail.com>
2023-06-28 21:05:17 +0100

    Adds more test to improve coverage- same % result, test passing

--- tests/test_conjured_item.py
+++ tests/test_conjured_item.py
@@ -9,3 +9,10 @@ def test_conjured_item_decreases_in_quality():
     gilded_rose.update_quality()
     assert items[0].quality == 19, "Quality of 'Conjured Mana Cake' should decrease by 1"
     assert items[0].sell_in == 14, "Sell in days should decrease by 1"
+
+def test_conjured_item_decreases_twice_as_fast_past_sell_date():
+    items = [ConjuredItem("Conjured Mana Cake", sell_in=0, quality=20)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == 16, "Quality of 'Conjured Mana Cake' should decrease by 4 when sell_in is less than 0"
+    assert items[0].sell_in == -1, "Sell in days should decrease by 1"
